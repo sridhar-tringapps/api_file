8b986479b42da32e93b59fce71c1865f005fd615
sridhar16899 <sridhar.arumugam@tringapps.com>
2021-04-15 15:49:13 +0530

    gemfile

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,6 @@
 class WelcomeController < ApplicationController
     def index
         @table = Welcome.all
-        render json: { Status:'success',message:'front end users list',data:@user},status: 200
+        render json: { Status:'success',message:'front end users list',data:@table},status: 200  
     end
 end
