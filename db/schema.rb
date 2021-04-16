e2f697fde45f6181a35c8d1e0115aafef415865c
sridhar16899 <sridhar.arumugam@tringapps.com>
2021-04-16 13:51:26 +0530

    pg gem

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2021_04_07_074253) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "tables", force: :cascade do |t|
     t.string "name"
     t.string "email"
